2421e25414beb3cc4bbb7b21267bb95138e4b44b
Umid Suleymanli <103460358+usuleymanli16281@users.noreply.github.com>
2022-04-27 22:20:00 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 window.onload = function() 
 { 
   document.getElementsByClassName('transition')
   [0].classList.add('translateAnimationClass'); 
 }
-=======
-window.onload = function() 
-{ 
-  document.getElementsByClassName('transition')
-  [0].classList.add('translateAnimationClass'); 
-}
->>>>>>> f0561870f7e690eb96b722a878925b4ba9f6c49d
